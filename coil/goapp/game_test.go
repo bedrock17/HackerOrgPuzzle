2fca45fffff1c4fda8f9ab441a440ba0a25cd059
SHK <bedrook@gmail.com>
2019-10-28 05:59:10 +0900

    테스트 코드

--- coil/goapp/game_test.go
+++ coil/goapp/game_test.go
@@ -9,6 +9,7 @@ import (
 // 게임코드 튜닝 시 검증 하기위한 코드
 
 func testOption() {
+	game.SetDebugMode(true)
 	game.SetGoTestMode(true)
 }
 func test(w, h int, board, answer string, t *testing.T) {
@@ -16,7 +17,6 @@ func test(w, h int, board, answer string, t *testing.T) {
 	testAnswer, i, j := game.GetSolution(w, h, board)
 
 	if testAnswer == answer {
-
 	} else {
 		t.Error(testAnswer, i, j)
 	}
@@ -29,6 +29,15 @@ func Test_Game50(t *testing.T) {
 	answer := "RDRDDRDDDLDRDRLDDLRDLR"
 	test(w, h, board, answer, t)
 }
+
+func Test_Game70(t *testing.T) {
+	w := 27
+	h := 26
+	board := "XX......XX.....XXX............X....X..XXX.XXX.X...XXX..X.....XX..........X.X..XX.....XX...XX...X.........XX.XXX..X......X.X....XX......XX...X..X.......XX.X...XXXXX..X.........X.......X.......XX...X.X...X.XX...XXX.............X.X...XX.X.....X..XXX...X.....X..XX......X......XX...X.X....X.....X..........X..X......X......X...X..X.....XX....XX....X........X...X.XXXX.....X..XX...X.....X.....X..XXXXX.XX.............X....X...XX.XX.....XXXXXXX...X....X....XXX.........X......X....XXXXXX.X.X.....X........X.........X.....XXXXXXX...X..X.XXX....X....XX..X...XXX.X..X...XX...X...X....XXXXX.X.......XX....X...XXXX...X.......X....X..X..XX....X.X..XX...........XX....X..X.X...X.XX...........X..X..X...X...XX......."
+	answer := "RUDRURRLDDDRDLRRDULDLDLDRRDRDLLDLDLLUURULL"
+	test(w, h, board, answer, t)
+}
+
 func Test_Game100(t *testing.T) {
 	w := 37
 	h := 36
